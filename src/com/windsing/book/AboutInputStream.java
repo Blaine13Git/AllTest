a27fef21a69c932cbd69c6cc9f443222e23f8ff3
Blaine13Git <windsingcc@163.com>
2017-04-07 20:37:45 +0800

    文件相关

--- src/com/windsing/book/AboutInputStream.java
+++ src/com/windsing/book/AboutInputStream.java
@@ -8,15 +8,20 @@ import java.io.FileNotFoundException;
  * 输入流，inputStream，读
  * 输出流，outputStream,写
  * 操作的对象为--字节序列--
+ * 
  */
 public class AboutInputStream {
 	
-
 	public static void main(String[] args) {
 		String pathname = "";
 		File file = new File(pathname);
 		try {
 			FileInputStream fis = new FileInputStream(file);
+			/**
+			 * 传入的参数如果是一个文件，会进行安全检查
+			 * 安全管理器是允许应用程序实施安全策略的类。
+			 * 它允许应用程序在执行可能不安全或敏感的操作之前确定操作是什么以及是否在允许执行操作的安全上下文中尝试。
+			 */
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
